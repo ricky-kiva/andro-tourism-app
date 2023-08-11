88b077304a6d411855fc0f6d788249b97477ddf4
ricky-kiva <ricky.kiva@gmail.com>
2023-08-11 17:45:42 +0700

    docs(data): comment on TourismRepository

--- app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
@@ -1,11 +1,13 @@
 package com.dicoding.tourismapp.core.data
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
 import com.dicoding.tourismapp.core.data.source.local.LocalDataSource
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
+import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.DataMapper
 
@@ -29,30 +31,50 @@ class TourismRepository private constructor(
             }
     }
 
-    fun getAllTourism(): LiveData<Resource<List<TourismEntity>>> =
-        object : NetworkBoundResource<List<TourismEntity>, List<TourismResponse>>(appExecutors) {
-            override fun loadFromDB(): LiveData<List<TourismEntity>> {
-                return localDataSource.getAllTourism()
+    // Get `Tourism` list from API & local database
+    fun getAllTourism(): LiveData<Resource<List<Tourism>>> =
+
+        // object to manage the flow of data betweek network & local storage
+        object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>(appExecutors) {
+
+            // called first. checks local database (Room), then map to domain `model`
+            override fun loadFromDB(): LiveData<List<Tourism>> {
+                return Transformations.map(localDataSource.getAllTourism()) {
+                    DataMapper.mapEntitiesToDomain(it)
+                }
             }
 
-            override fun shouldFetch(data: List<TourismEntity>?): Boolean =
+            // determine network call is necessary based on data retrieved from local database
+            // will check data is null or empty to determine fetch or not
+            override fun shouldFetch(data: List<Tourism>?): Boolean =
                 data == null || data.isEmpty()
 
+            // if shouldFetch() is true, this function called to get data from remote source
             override fun createCall(): LiveData<ApiResponse<List<TourismResponse>>> =
                 remoteDataSource.getAllTourism()
 
+            // executed after remote fetch success. map response to entities then add to local source
             override fun saveCallResult(data: List<TourismResponse>) {
                 val tourismList = DataMapper.mapResponsesToEntities(data)
                 localDataSource.insertTourism(tourismList)
             }
+
+        // NetworkBoundResource diagram: https://d17ivq9b7rppb3.cloudfront.net/original/academy/20200812165119cf5c79dfc223d9f04f39899ddfc29700.png
         }.asLiveData()
 
-    fun getFavoriteTourism(): LiveData<List<TourismEntity>> {
-        return localDataSource.getFavoriteTourism()
+    // get favorite `tourism` list
+    // use `domain` to be `presented`
+    fun getFavoriteTourism(): LiveData<List<Tourism>> {
+        return Transformations.map(localDataSource.getFavoriteTourism()) {
+            DataMapper.mapEntitiesToDomain(it)
+        }
     }
 
-    fun setFavoriteTourism(tourism: TourismEntity, state: Boolean) {
-        appExecutors.diskIO().execute { localDataSource.setFavoriteTourism(tourism, state) }
+    // set favorite `tourism`
+    // use `entity` to be sent to `DAO`
+    fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
+        val tourismEntity = DataMapper.mapDomainToEntity(tourism)
+        appExecutors.diskIO().execute { localDataSource.setFavoriteTourism(tourismEntity, state) }
     }
 }
 
