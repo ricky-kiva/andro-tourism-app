a8fad695a6a5dd835bc20e2641ed188f06155710
ricky-kiva <ricky.kiva@gmail.com>
2023-08-11 16:02:42 +0700

    feat DataMapper methods

--- app/src/main/java/com/dicoding/tourismapp/core/utils/DataMapper.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/utils/DataMapper.kt
@@ -2,8 +2,11 @@ package com.dicoding.tourismapp.core.utils
 
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
+import com.dicoding.tourismapp.core.domain.model.Tourism
 
 object DataMapper {
+
+    // to map API responses to entity
     fun mapResponsesToEntities(input: List<TourismResponse>): List<TourismEntity> {
         val tourismList = ArrayList<TourismEntity>()
         input.map {
@@ -22,4 +25,33 @@ object DataMapper {
         }
         return tourismList
     }
+
+    // to convert entities to domain
+    fun mapEntitiesToDomain(input: List<TourismEntity>): List<Tourism> =
+        input.map {
+            Tourism(
+                tourismId = it.tourismId,
+                description = it.description,
+                name = it.name,
+                address = it.address,
+                latitude = it.latitude,
+                longitude = it.longitude,
+                like = it.like,
+                image = it.image,
+                isFavorite = it.isFavorite
+            )
+        }
+
+    // to convert domain `model` to entity
+    fun mapDomainToEntity(input: Tourism) = TourismEntity(
+        tourismId = input.tourismId,
+        description = input.description,
+        name = input.name,
+        address = input.address,
+        latitude = input.latitude,
+        longitude = input.longitude,
+        like = input.like,
+        image = input.image,
+        isFavorite = input.isFavorite
+    )
 }
