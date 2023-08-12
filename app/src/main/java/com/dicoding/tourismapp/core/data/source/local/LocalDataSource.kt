6d288858939f3c0e2d9f3aae641f91d782de6a0d
ricky-kiva <ricky.kiva@gmail.com>
2023-08-12 19:49:23 +0700

    feat(data-local): implement Coroutines Flow

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/local/LocalDataSource.kt
@@ -3,6 +3,7 @@ package com.dicoding.tourismapp.core.data.source.local
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.data.source.local.room.TourismDao
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 class LocalDataSource private constructor(private val tourismDao: TourismDao) {
 
@@ -15,11 +16,19 @@ class LocalDataSource private constructor(private val tourismDao: TourismDao) {
             }
     }
 
+    fun getAllTourism(): Flow<List<TourismEntity>> = tourismDao.getAllTourism()
+
+    fun getFavoriteTourism(): Flow<List<TourismEntity>> = tourismDao.getFavoriteTourism()
+
+    // use suspend because it's a database operation
+    suspend fun insertTourism(tourismList: List<TourismEntity>) = tourismDao.insertTourism(tourismList)
+
+    /* RxJava
     fun getAllTourism(): Flowable<List<TourismEntity>> = tourismDao.getAllTourism()
 
     fun getFavoriteTourism(): Flowable<List<TourismEntity>> = tourismDao.getFavoriteTourism()
 
-    fun insertTourism(tourismList: List<TourismEntity>) = tourismDao.insertTourism(tourismList)
+    fun insertTourism(tourismList: List<TourismEntity>) = tourismDao.insertTourism(tourismList)*/
 
     fun setFavoriteTourism(tourism: TourismEntity, newState: Boolean) {
         tourism.isFavorite = newState
--- app/src/main/java/com/dicoding/tourismapp/core/data/source/local/room/TourismDao.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/local/room/TourismDao.kt
@@ -4,10 +4,24 @@ import androidx.room.*
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import io.reactivex.Completable
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TourismDao {
 
+    @Query("SELECT * FROM tourism")
+    fun getAllTourism(): Flow<List<TourismEntity>>
+
+    @Query("SELECT * FROM tourism where isFavorite = 1")
+    fun getFavoriteTourism(): Flow<List<TourismEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTourism(tourism: List<TourismEntity>) // use suspend because it's a database operation
+
+    @Update
+    fun updateFavoriteTourism(tourism: TourismEntity)
+
+    /* RxJava
     @Query("SELECT * FROM tourism")
     fun getAllTourism(): Flowable<List<TourismEntity>>
 
@@ -18,5 +32,5 @@ interface TourismDao {
     fun insertTourism(tourism: List<TourismEntity>): Completable
 
     @Update
-    fun updateFavoriteTourism(tourism: TourismEntity)
+    fun updateFavoriteTourism(tourism: TourismEntity)*/
 }
