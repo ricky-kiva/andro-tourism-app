0d0fb693f77f3c6515b6b64fa181d569c4857802
ricky-kiva <ricky.kiva@gmail.com>
2023-08-12 19:53:30 +0700

    feat(presentation-viewModel): implement Coroutines Flow

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -2,11 +2,15 @@ package com.dicoding.tourismapp.favorite
 
 import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class FavoriteViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
 
+    val favoriteTourism = tourismUseCase.getFavoriteTourism().asLiveData()
+
+    /* RxJava
     // `LiveDataReactiveStreams` convert reactive stream into livedata
-    val favoriteTourism = LiveDataReactiveStreams.fromPublisher(tourismUseCase.getFavoriteTourism())
+    val favoriteTourism = LiveDataReactiveStreams.fromPublisher(tourismUseCase.getFavoriteTourism())*/
 
 }
--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -2,11 +2,15 @@ package com.dicoding.tourismapp.home
 
 import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class HomeViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
 
+    val tourism = tourismUseCase.getAllTourism().asLiveData()
+
+    /* RxJava
     // `LiveDataReactiveStreams` convert reactive stream into livedata
-    val tourism = LiveDataReactiveStreams.fromPublisher(tourismUseCase.getAllTourism())
+    val tourism = LiveDataReactiveStreams.fromPublisher(tourismUseCase.getAllTourism())*/
 
 }
