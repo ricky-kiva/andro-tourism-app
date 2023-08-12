be57748f92bd1207ce9293638509ce7d0e1e3057
ricky-kiva <ricky.kiva@gmail.com>
2023-08-12 19:52:57 +0700

    feat(domain-useCase): implement Coroutines Flow

--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismUseCase.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismUseCase.kt
@@ -3,13 +3,21 @@ package com.dicoding.tourismapp.core.domain.usecase
 import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 interface TourismUseCase {
 
+    fun getAllTourism(): Flow<Resource<List<Tourism>>>
+
+    fun getFavoriteTourism(): Flow<List<Tourism>>
+
+    fun setFavoriteTourism(tourism: Tourism, state: Boolean)
+
+    /* RxJava
     fun getAllTourism(): Flowable<Resource<List<Tourism>>>
 
     fun getFavoriteTourism(): Flowable<List<Tourism>>
 
-    fun setFavoriteTourism(tourism: Tourism, state: Boolean)
+    fun setFavoriteTourism(tourism: Tourism, state: Boolean)*/
 
 }
--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/ToursimInteractor.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/ToursimInteractor.kt
@@ -4,13 +4,22 @@ import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 class TourismInteractor(private val tourismRepository: ITourismRepository): TourismUseCase {
+
+    override fun getAllTourism(): Flow<Resource<List<Tourism>>> =
+        tourismRepository.getAllTourism()
+
+    override fun getFavoriteTourism(): Flow<List<Tourism>> =
+        tourismRepository.getFavoriteTourism()
+
+    /* RxJava
     override fun getAllTourism(): Flowable<Resource<List<Tourism>>> =
         tourismRepository.getAllTourism()
 
     override fun getFavoriteTourism(): Flowable<List<Tourism>> =
-        tourismRepository.getFavoriteTourism()
+        tourismRepository.getFavoriteTourism()*/
 
     override fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
         tourismRepository.setFavoriteTourism(tourism, state)
