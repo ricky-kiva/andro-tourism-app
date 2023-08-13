4e156b758da71594a3410039c5a45bd6fa9b8c66
ricky-kiva <ricky.kiva@gmail.com>
2023-08-13 15:13:38 +0700

    feat(scope-application): start Koin on application create

---
+++ app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
@@ -0,0 +1,31 @@
+package com.dicoding.tourismapp
+
+import android.app.Application
+import com.dicoding.tourismapp.core.di.databaseModule
+import com.dicoding.tourismapp.core.di.networkModule
+import com.dicoding.tourismapp.core.di.repositoryModule
+import com.dicoding.tourismapp.core.di.useCaseModule
+import com.dicoding.tourismapp.core.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.NONE) // set no logging for Koin
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
+    }
+}
